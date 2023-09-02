29112077eb5af9e80f1b662e1698ead6559c079f
tobias.carlbom@gmail.com <tobias.carlbom@gmail.com>
2023-09-02 09:38:33 +0000

    Created Form2; Edited settings

---
+++ client_code/Form2/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Form2Template
+from anvil import *
+
+class Form2(Form2Template):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+        # Any code you write here will run before the form opens.
+
+    def button_calc_click(self, **event_args):
+        power = float(self.text_box_power.text)
+        hours = float(self.text_box_hours.text)
+        energy_raw = power * hours
+        energy = round(energy_raw, 2)
+        self.label_energy.text = f"""After {hours} hours, the battery will be charged with {energy} kWh"""
