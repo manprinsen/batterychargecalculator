714018ac2082aea6f65f6107497fd5dd20323d09
tobias.carlbom@gmail.com <tobias.carlbom@gmail.com>
2023-09-05 19:40:42 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -9,6 +9,12 @@ class Form2(Form2Template):
         # Any code you write here will run before the form opens.
 
     def button_calc_click(self, **event_args):
+        if self.text_box_hours.text == None:
+            alert("Please enter charge time")
+            return
+        if self.text_box_power.text == None:
+            alert("Please enter charge speed")
+            return
         power = float(self.text_box_power.text)
         hours = float(self.text_box_hours.text)
         energy_raw = power * hours
